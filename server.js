11b844ccee1c6af73534fbb2a14b2adfc7005b3d
myztajay <myztajay@hotmail.com>
2017-10-28 17:20:39 -0400

    Added in client side and server functionality to transmit msgs and add usernames to those messages

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const server = require('http').createServer(app);
 const path = require('path');
 const io = require('socket.io').listen(server);
+const usernames = [];
 
 //enivronmental variables
 server.listen(process.env.PORT || 3000);
@@ -14,14 +15,35 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req, res)=>{
   res.sendFile(__dirname + '/index.html');
-  console.log(__dirname);
 });
 
 
 io.sockets.on('connection', (socket)=>{
   console.log('connected to socket');
-
+  
+  socket.on('new user', (data, callback)=>{
+    usernames.indexOf(data) != -1 ? callback(false) : callback(true);
+      socket.username = data;
+      usernames.push(socket.username);
+      updateUsernames(); 
+  });
+  
+  //Update Usernames
+  updateUsernames = () =>{
+    io.sockets.emit('usernames', usernames);
+  };
+  // Send Message
   socket.on('send message', (data)=>{
-    io.sockets.emit('new message', {msg: data});
+    console.log('this was triggers');
+    io.sockets.emit('new message', {msg: data, user: socket.username});
   });  
+  
+  socket.on('disconnect', (data)=>{
+    if(!socket.username){
+      return;
+    }  
+    usernames.splice(usernames.indexOf(socket.username), 1);
+    updateUsernames();
+  });
 });
+
