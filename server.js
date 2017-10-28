c6e9cb112b801008d21adcba3f9ad4ea970f7250
myztajay <myztajay@hotmail.com>
2017-10-28 15:27:25 -0400

    Added path to css

--- server.js
+++ server.js
@@ -4,13 +4,24 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+const path = require('path');
 const io = require('socket.io').listen(server);
 
 //enivronmental variables
 server.listen(process.env.PORT || 3000);
-app.use(express.static(__dirname + '/public'));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req, res)=>{
   res.sendFile(__dirname + '/index.html');
+  console.log(__dirname);
 });
 
+
+io.sockets.on('connection', (socket)=>{
+  console.log('connected to socket');
+
+  socket.on('send message', (data)=>{
+    io.sockets.emit('new message', {msg: data});
+  });  
+});
